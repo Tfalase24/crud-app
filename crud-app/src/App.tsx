1f334a4ca919bad1ecd229c70800612f0c1f8f84
TF <tfalase24@gmail.com>
2023-03-06 22:15:53 +0000

    feat(src\App.tsx): Added element variable

--- crud-app/src/App.tsx
+++ crud-app/src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return element;
 }
 
 export default App;
+
+function printName(user: User): string {
+  return user.firstName + " " + user.lastName;
+}
+
+interface User {
+  firstName: string;
+  lastName: string;
+}
+
+const user: User = {
+  firstName: "Adetito",
+  lastName: "Falase",
+};
+
+const element: JSX.Element = (
+  <div>
+    <h1> Hello {printName(user)}</h1>
+    <h2> This is my react app which I am using to learn react</h2>
+  </div>
+);
