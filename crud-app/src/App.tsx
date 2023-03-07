d4a8392ff1ab03a5a026cf00510e40c9d5fbf01c
TF <tfalase24@gmail.com>
2023-03-07 00:36:29 +0000

    feat(crud-app\src): Index path can't end with .tsx

--- crud-app/src/App.tsx
+++ crud-app/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Welcome from "./components/Welcome.tsx";
+import Welcome from "./components/Welcome";
 
 function App() {
   return element;
