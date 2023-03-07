fb0f0b3c21c97c668c5cf04e865b6af1bfeb5a6b
TF <tfalase24@gmail.com>
2023-03-07 00:27:08 +0000

    feat(crud-app\src\App.tsx): Added picture and description to object and rendered

--- crud-app/src/App.tsx
+++ crud-app/src/App.tsx
@@ -15,16 +15,23 @@ function printName(user: User): string {
 interface User {
   firstName: string;
   lastName: string;
+  urlOfPicture?: string;
+  descriptionOfPicture?: string;
 }
 
 const user: User = {
   firstName: "Adetito",
   lastName: "Falase",
+  urlOfPicture:
+    "https://media.euobserver.com/75ba88da8a99cadb376a759df8710279.jpg",
+  descriptionOfPicture: "Queen Lizzie and I sipping Cherry B's... Four minimum",
 };
 
 const element: JSX.Element = (
   <div>
     <h1> Hello {printName(user)}</h1>
     <h2> This is my react app which I am using to learn react</h2>
+    <img src={user.urlOfPicture} width={200}></img>
+    <h3>{user.descriptionOfPicture}</h3>
   </div>
 );
