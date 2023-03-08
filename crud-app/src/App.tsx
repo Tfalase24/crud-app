3dc5bfc3df3ab3c888028a55850dee5edf668513
TF <tfalase24@gmail.com>
2023-03-08 19:02:52 +0000

    feat(crud-app\src\App.tsx): Added button that uses state variable, useState

--- crud-app/src/App.tsx
+++ crud-app/src/App.tsx
@@ -1,17 +1,40 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 import Welcome from "./components/Welcome";
 import User from "./interfaces/User";
 
 function App(): JSX.Element {
-  return element;
+  const [clicks, setClicks] = useState(0);
+  return (
+    <div>
+      <Welcome firstName={user.firstName} lastName={user.lastName} />
+      <h1> Hello {printName(user)}</h1>
+      <h2> This is my react app which I am using to learn react</h2>
+      <img src={user.urlOfPicture} width={200} alt="Island Boys"></img>
+      <h3>{user.descriptionOfPicture}</h3>
+      <h4>{button}</h4>
+      <button
+        onClick={() => {
+          setClicks(clicks + 1);
+        }}
+      >
+        {" "}
+        This is a button to literally just count how many Cherry B's have been
+        sipped on. Current {clicks} many Cherry B's
+      </button>
+    </div>
+  );
 }
 
 export default App;
 
 function printName(user: User): string {
   return user.firstName + " " + user.lastName;
+}
+
+function screamFunction(e: React.MouseEvent<HTMLButtonElement>): void {
+  // e.preventDefault();
+  console.log("Reeeeeeeeek!!!!");
 }
 
 const user: User = {
@@ -22,12 +45,8 @@ const user: User = {
   descriptionOfPicture: "Queen Lizzie and I sipping Cherry B's... Four minimum",
 };
 
-const element: JSX.Element = (
+const button: JSX.Element = (
   <div>
-    <Welcome firstName={user.firstName} lastName={user.lastName} />
-    <h1> Hello {printName(user)}</h1>
-    <h2> This is my react app which I am using to learn react</h2>
-    <img src={user.urlOfPicture} width={200}></img>
-    <h3>{user.descriptionOfPicture}</h3>
+    <button onClick={screamFunction}> Click here</button>
   </div>
 );
