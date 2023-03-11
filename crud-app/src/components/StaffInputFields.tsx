19e6b1a45546fc87dc91888780e814b630980fb9
TF <tfalase24@gmail.com>
2023-03-11 11:27:24 +0000

    feat(src\components\StaffInputFields.tsx): Created a staff input field

---
+++ crud-app/src/components/StaffInputFields.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { useState } from "react";
+import { DisplayEmail } from "./DisplayEmail";
+
+const StaffInputFields = () => {
+  const [staff, setStaff] = useState({
+    firstName: "",
+    lastName: "",
+    emailAddress: "",
+  });
+
+  function onChangeFirstName(e: React.ChangeEvent<HTMLInputElement>) {
+    setStaff({ ...staff, firstName: e.target.value });
+  }
+
+  function onChangeLastName(e: React.ChangeEvent<HTMLInputElement>) {
+    setStaff({ ...staff, lastName: e.target.value });
+  }
+  return (
+    <div>
+      <input
+        value={staff.firstName}
+        onChange={onChangeFirstName}
+        placeholder="Enter your first name"
+      ></input>
+      <input
+        value={staff.lastName}
+        onChange={onChangeLastName}
+        placeholder="Enter your last name"
+      ></input>
+      <DisplayEmail firstName={staff.firstName} lastName={staff.lastName} />
+    </div>
+  );
+};
+
+export default StaffInputFields;
